c04a93ade972c9b4212f9e30cf019b0a68682d4b
Joseph George Nessim <josephgeorgenessim@gmail.com>
2026-03-29 20:29:16 +0200

    setup project and implement navigation

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/Header";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "ConinPluse",
+  description: "CoinPulse is a crypto tracking platform",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className="dark"
+    >
+      <body className="min-h-full flex flex-col">
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
